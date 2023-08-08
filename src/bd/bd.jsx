031149f57b65667003ce82fcaaa99e373dac3b45
CipherSeeker <140613065+CipherSeeker@users.noreply.github.com>
2023-08-09 00:46:54 +0300

    SweetChest
    Added Sweet Chest project to portfolio

--- src/bd/bd.jsx
+++ src/bd/bd.jsx
@@ -15,6 +15,13 @@ import lv5 from "../assets/img/LittleVoices/Post.avif";
 import lv6 from "../assets/img/LittleVoices/Search.avif";
 import lv7 from "../assets/img/LittleVoices/Trending.avif";
 import lv8 from "../assets/img/LittleVoices/ScoreLV.avif";
+import sc1 from "../assets/img/SweetChest/Hero.avif";
+import sc2 from "../assets/img/SweetChest/Section.avif";
+import sc3 from "../assets/img/SweetChest/Section-2.avif";
+import sc4 from "../assets/img/SweetChest/Section-3.avif";
+import sc5 from "../assets/img/SweetChest/Section-4.avif";
+import sc6 from "../assets/img/SweetChest/Section-5.avif";
+import sc7 from "../assets/img/SweetChest/Footer.avif";
 
 export default [
   {
@@ -28,11 +35,49 @@ export default [
     urlgit: `https://github.com/CipherSeeker/portfolio-project`,
     about: (
       <>
-        <p>Welcome to the "Runers Biome" – a project that holds a special place in my developers portfolio. As a passionate developer with a love for both coding and fitness, I embarked on creating this web platform to bring together a community of running enthusiasts from around the world. This project is a testament to my dedication to combining my technical skills with my personal interests.</p>
-        <p>The Running Enthusiast Hub is not just another website; it's a dynamic space where runners of all levels can find inspiration, valuable resources, and connect with like-minded individuals. From beginners taking their first strides to seasoned marathoners chasing personal bests, this platform is designed to cater to every runner's needs. The website features a user-friendly interface, allowing members to access training plans, nutrition tips, injury prevention strategies, and a collection of motivational stories shared by fellow runners.</p>
-        <p>One of the highlights of this project is the interactive route mapping feature, powered by cutting-edge technology, enabling users to discover new running routes in their local areas or when traveling. Additionally, a community forum fosters engaging discussions on training techniques, gear reviews, and upcoming running events, creating a sense of camaraderie among users who share a common passion.</p>
-        <p>As a developer, I embraced the challenge of blending aesthetics with functionality. The Running Enthusiast Hub boasts a responsive design that ensures seamless user experience across various devices. From coding the back-end for data management to crafting captivating front-end visuals, every aspect of this project reflects my commitment to detail and excellence.</p>
-        <p>I invite you to explore the Running Enthusiast Hub and witness the harmony of technology and running fervor. This project not only showcases my technical prowess but also embodies my dedication to creating digital spaces that cater to the diverse interests of individuals worldwide. Thank you for joining me on this exhilarating journey of code and miles.</p>
+        <p>
+          Welcome to the "Runers Biome" – a project that holds a special place
+          in my developers portfolio. As a passionate developer with a love for
+          both coding and fitness, I embarked on creating this web platform to
+          bring together a community of running enthusiasts from around the
+          world. This project is a testament to my dedication to combining my
+          technical skills with my personal interests.
+        </p>
+        <p>
+          The Running Enthusiast Hub is not just another website; it's a dynamic
+          space where runners of all levels can find inspiration, valuable
+          resources, and connect with like-minded individuals. From beginners
+          taking their first strides to seasoned marathoners chasing personal
+          bests, this platform is designed to cater to every runner's needs. The
+          website features a user-friendly interface, allowing members to access
+          training plans, nutrition tips, injury prevention strategies, and a
+          collection of motivational stories shared by fellow runners.
+        </p>
+        <p>
+          One of the highlights of this project is the interactive route mapping
+          feature, powered by cutting-edge technology, enabling users to
+          discover new running routes in their local areas or when traveling.
+          Additionally, a community forum fosters engaging discussions on
+          training techniques, gear reviews, and upcoming running events,
+          creating a sense of camaraderie among users who share a common
+          passion.
+        </p>
+        <p>
+          As a developer, I embraced the challenge of blending aesthetics with
+          functionality. The Running Enthusiast Hub boasts a responsive design
+          that ensures seamless user experience across various devices. From
+          coding the back-end for data management to crafting captivating
+          front-end visuals, every aspect of this project reflects my commitment
+          to detail and excellence.
+        </p>
+        <p>
+          I invite you to explore the Running Enthusiast Hub and witness the
+          harmony of technology and running fervor. This project not only
+          showcases my technical prowess but also embodies my dedication to
+          creating digital spaces that cater to the diverse interests of
+          individuals worldwide. Thank you for joining me on this exhilarating
+          journey of code and miles.
+        </p>
       </>
     ),
   },
@@ -47,20 +92,117 @@ export default [
     urlgit: "https://github.com/CipherSeeker/Little-Voices",
     about: (
       <>
-        <p>Welcome to the "Little Voices" – a project that holds a special place in my developer's portfolio. As a dedicated developer with a passion for creating impactful digital solutions, I embarked on this endeavor to provide a comprehensive online platform for individuals navigating the beautiful journey of pregnancy. This project stands as a testament to my commitment to merging technical expertise with my desire to make a positive difference in people's lives.</p>
-        <p>The Little Voices Blog ranscends the realm of ordinary blogs; it serves as a dynamic haven customized to aid individuals at every phase of pregnancy. From the early stages of family planning to the multifaceted trimesters of pregnancy, this platform is meticulously designed to be a steadfast companion throughout this transformative voyage. The user-friendly interface serves as a gateway to a plethora of resources: prenatal care guidance, expert-authored articles, nutritional wisdom, and personal stories shared by fellow travelers.</p>
-        <p>A standout aspect of this project is the interactive community engagement, fostering meaningful conversations that empower users to connect, share their experiences, and seek wisdom from those who comprehend the intricacies of this life-altering journey.</p>
-        <p>As a developer, I embraced the challenge of designing for empathy and inclusivity. The Pregnancy Journey Portal boasts a user-centric design, ensuring seamless navigation and accessibility across diverse devices. From crafting an efficient backend for data management to creating engaging front-end features, every facet of this project embodies my dedication to delivering excellence.</p>
-        <p>I invite you to immerse yourself in the Pregnancy Insights Blog and witness the seamless fusion of technology and the profound expedition of pregnancy. This project is a testament not only to my technical prowess but also to my commitment to creating digital solutions that cater to the diverse needs of individuals worldwide. Thank you for joining me on this meaningful journey, where code and the miracle of life converge in exquisite harmony.</p>
+        <p>
+          Welcome to the "Little Voices" – a project that holds a special place
+          in my developer's portfolio. As a dedicated developer with a passion
+          for creating impactful digital solutions, I embarked on this endeavor
+          to provide a comprehensive online platform for individuals navigating
+          the beautiful journey of pregnancy. This project stands as a testament
+          to my commitment to merging technical expertise with my desire to make
+          a positive difference in people's lives.
+        </p>
+        <p>
+          The Little Voices Blog ranscends the realm of ordinary blogs; it
+          serves as a dynamic haven customized to aid individuals at every phase
+          of pregnancy. From the early stages of family planning to the
+          multifaceted trimesters of pregnancy, this platform is meticulously
+          designed to be a steadfast companion throughout this transformative
+          voyage. The user-friendly interface serves as a gateway to a plethora
+          of resources: prenatal care guidance, expert-authored articles,
+          nutritional wisdom, and personal stories shared by fellow travelers.
+        </p>
+        <p>
+          A standout aspect of this project is the interactive community
+          engagement, fostering meaningful conversations that empower users to
+          connect, share their experiences, and seek wisdom from those who
+          comprehend the intricacies of this life-altering journey.
+        </p>
+        <p>
+          As a developer, I embraced the challenge of designing for empathy and
+          inclusivity. The Pregnancy Journey Portal boasts a user-centric
+          design, ensuring seamless navigation and accessibility across diverse
+          devices. From crafting an efficient backend for data management to
+          creating engaging front-end features, every facet of this project
+          embodies my dedication to delivering excellence.
+        </p>
+        <p>
+          I invite you to immerse yourself in the Pregnancy Insights Blog and
+          witness the seamless fusion of technology and the profound expedition
+          of pregnancy. This project is a testament not only to my technical
+          prowess but also to my commitment to creating digital solutions that
+          cater to the diverse needs of individuals worldwide. Thank you for
+          joining me on this meaningful journey, where code and the miracle of
+          life converge in exquisite harmony.
+        </p>
       </>
     ),
   },
   {
     id: "3",
-    img: `${photo2}`,
-    title: "Project 3",
-    skill: ["Next.JS", "React", "HTML", "CSS"],
+    img: `${sc1}`,
+    title: "Sweet Chest",
+    skill: ["JavaScript", "HTML", "CSS"],
     cms: "WordPress",
+    images: [sc1, sc2, sc3, sc4, sc5, sc6, sc7],
+    url: "https://cipherseeker.github.io/Sweet-Chest/",
+    urlgit: "https://github.com/CipherSeeker/Sweet-Chest",
+    about: (
+      <>
+        <p>
+          Welcome to the "Sweet Chest" – a standout gem in my developer's
+          portfolio. As an avid developer, I've taken on the task of crafting a
+          captivating online platform dedicated to the art of selling delectable
+          pastries. This project showcases my ability to seamlessly weave
+          technical finesse with a scrumptious objective – providing a virtual
+          haven for those seeking to indulge in the world of exquisite cakes and
+          cupcakes.
+        </p>
+        <p>
+          The Sweet Chest is more than just a website; it's a dynamic digital
+          confectionery that caters to both the connoisseurs of baked goods and
+          the casual dessert enthusiasts. From those in search of the perfect
+          treat for special occasions to individuals simply craving a sweet
+          escape, this platform is designed to be a delightful online
+          destination that leaves taste buds tingling. The user-friendly
+          interface opens doors to a tantalizing array of offerings: sumptuous
+          cakes adorned with intricate designs, delectable cupcakes bursting
+          with flavor, and a gallery that showcases the edible artistry crafted
+          in our bakery.
+        </p>
+        <p>
+          A standout aspect of this project is the interactive community
+          engagement, fostering meaningful conversations that empower users to
+          connect, share their experiences, and seek wisdom from those who
+          comprehend the intricacies of this life-altering journey.
+        </p>
+        <p>
+          A highlight of this project is the seamless online shopping
+          experience, where customers can effortlessly select their desired
+          treats, customize orders, and have their delightful indulgences
+          delivered to their doorstep. From choosing flavors to personalizing
+          decorations, this platform enhances the process of acquiring these
+          edible masterpieces.
+        </p>
+        <p>
+          As a developer, I embraced the challenge of fusing aesthetics with
+          functionality. The Sweet Delights Bakery website boasts an interface
+          that's as visually appetizing as the treats it offers, ensuring
+          seamless navigation and accessibility across diverse devices. From
+          optimizing the backend for streamlined order processing to creating a
+          visually enticing frontend, every element of this project mirrors my
+          unwavering commitment to delivering excellence.
+        </p>
+        <p>
+          I invite you to explore the Sweet Delights Bakery and experience the
+          delightful synergy of technology and the art of baking. This project
+          not only demonstrates my technical prowess but also reflects my
+          dedication to crafting digital solutions that cater to the diverse
+          desires of individuals across the globe. Thank you for joining me on
+          this delightful journey, where code meets the sweetness of life's
+          pleasures.
+        </p>
+      </>
+    ),
   },
   {
     id: "4",
